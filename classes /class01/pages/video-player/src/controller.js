9be0a6342d23309c1a8ca737dfa1dc404ac60e3e
YohanBZ <barbozayohan@gmail.com>
2023-03-02 17:15:17 -0300

    class one finished

--- classes /class01/pages/video-player/src/controller.js
+++ classes /class01/pages/video-player/src/controller.js
@@ -1,9 +1,13 @@
 export default class Controller {
   #view;
-  #service;
-  constructor({ view, service }) {
+  #camera;
+  #worker;
+  #blinkCounter = 0;
+  constructor({ view, worker, camera }) {
     this.#view = view;
-    this.#service = service;
+    this.#camera = camera;
+    this.#worker = this.#configureWorker(worker);
+
     this.#view.configureOnBtnClick(this.onBtnStart.bind(this));
   }
   static async initialize(deps) {
@@ -11,13 +15,47 @@ export default class Controller {
     controller.log('not yet detecting eye blink! click in the button to start');
     return controller.init();
   }
+  #configureWorker(worker) {
+    let ready = false;
+    worker.onmessage = ({ data }) => {
+      if ('READY' === data) {
+        console.log('worker is ready!');
+        this.#view.enableButton();
+        ready = true;
+        return;
+      }
+      const blinked = data.blinked;
+      this.#blinkCounter += blinked;
+      this.#view.togglePlayVideo();
+      console.log('blinked', blinked);
+    };
+
+    return {
+      send(msg) {
+        if (!ready) return;
+        worker.postMessage(msg);
+      },
+    };
+  }
   async init() {
     console.log('init');
   }
+
+  loop() {
+    const video = this.#camera.video;
+    const img = this.#view.getVideoFrame(video);
+    this.#worker.send(img);
+    this.log('detecting eye blink...');
+
+    setTimeout(() => this.loop(), 100);
+  }
   log(text) {
-    this.#view.log(`logger: ${text}`);
+    const times = `    -blinked times:${this.#blinkCounter}`;
+    this.#view.log(`status: ${text}`.concat(this.#blinkCounter ? times : ''));
   }
   onBtnStart() {
     this.log('initializing detection...');
+    this.#blinkCounter = 0;
+    this.loop();
   }
 }
--- classes /class01/pages/video-player/src/worker.js
+++ classes /class01/pages/video-player/src/worker.js
@@ -3,7 +3,7 @@ import 'https://unpkg.com/@tensorflow/tfjs-converter@2.4.0/dist/tf-converter.js'
 import 'https://unpkg.com/@tensorflow/tfjs-backend-webgl@2.4.0/dist/tf-backend-webgl.js';
 import 'https://unpkg.com/@tensorflow-models/face-landmarks-detection@0.0.1/dist/face-landmarks-detection.js';
 
-import Service from './service';
+import Service from './service.js';
 // no processo principal é window
 // no worker é self
 
@@ -18,7 +18,8 @@ await service.loadModel();
 console.log('tf model loaded!');
 postMessage('READY');
 
-onmessage = ({ data }) => {
-  console.log('worker', data);
-  postMessage({ ok: 'ok' });
+onmessage = async ({ data: video }) => {
+  const blinked = await service.handBlinked(video);
+  if (!blinked) return;
+  postMessage({ blinked });
 };
